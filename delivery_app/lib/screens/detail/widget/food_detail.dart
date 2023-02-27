fd938baabd338b0d3fa40f4d0d29051ee5fc1e74
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-02-27 14:53:07 +0100

    Create food_quantity class

--- delivery_app/lib/screens/detail/widget/food_detail.dart
+++ delivery_app/lib/screens/detail/widget/food_detail.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../../constants/colors.dart';
 import '../../../models/food.dart';
+import 'food_quantity.dart';
 class FoodDetail extends StatelessWidget {
   final Food food;
   FoodDetail(this.food);
@@ -18,17 +19,17 @@ class FoodDetail extends StatelessWidget {
               fontWeight: FontWeight.bold,
             ),
             ),
-          const SizedBox(height: 10,),
+          const SizedBox(height: 15,),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               _buildIconText(Icons.access_time_outlined, Colors.blue, food.waitTime),
               _buildIconText(Icons.star_outlined, Colors.amber, food.score.toString()),
               _buildIconText(Icons.local_fire_department_outlined, Colors.red, food.energy.toString()),
-
             ],
-
-          )
+          ),
+          SizedBox(height: 30,),
+          FoodQuantity(food),
         ],
 
 
---
+++ delivery_app/lib/screens/detail/widget/food_quantity.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import '../../../models/food.dart';
+class FoodQuantity extends StatelessWidget {
+  final Food food;
+
+  FoodQuantity(this.food);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
