d942d615a9300a6ac7bfa74fce98dd096950942e
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-02-27 09:27:25 +0100

    Implement smooth page indicator in home pageÃ

--- delivery_app/lib/screens/home/home.dart
+++ delivery_app/lib/screens/home/home.dart
@@ -2,6 +2,7 @@ import 'package:delivery_app/constants/colors.dart';
 import 'package:delivery_app/widgets/food_list.dart';
 import 'package:delivery_app/widgets/restaurant_info.dart';
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../../models/restaurant.dart';
 import '../../widgets/custom_app_bar.dart';
@@ -46,6 +47,35 @@ class _HomePageState extends State<HomePage> {
                 },
               pageController,
               restaurant
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            height: 60,
+            child: SmoothPageIndicator(
+              controller: pageController,
+              count:restaurant.menu.length,
+              effect: CustomizableEffect(
+                dotDecoration: DotDecoration(
+                  width: 10,
+                  height: 10,
+                  color: Colors.grey.withOpacity(0.4),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                activeDotDecoration: DotDecoration(
+                  width: 12,
+                  height: 12,
+                  color: kBackground,
+                  borderRadius: BorderRadius.circular(10),
+                  dotBorder: const DotBorder(
+                    color: kPrimaryColor,
+                    width: 2,
+                  ),
+                ),
+              ),
+              onDotClicked: (index){
+                pageController.jumpToPage(index);
+              },
             ),
           )
         ],
