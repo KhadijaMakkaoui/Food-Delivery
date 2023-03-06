6355c0b3290543dec6ae2e1831f065f898c47c4e
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-03-06 16:50:34 +0100

    add green color

--- delivery_app/lib/constants/colors.dart
+++ delivery_app/lib/constants/colors.dart
@@ -4,3 +4,4 @@ import 'package:flutter/material.dart';
 const Color kPrimaryColor = Color(0xFFFDBF30);*/
 const Color kBackground = Color(0xFFFBE9E7);
 const Color kPrimaryColor =Color(0xFFFFCCBC);
+const Color kGreen = Color(0xFF539E83);
