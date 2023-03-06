32b21f2b836f75c59570a880a9604aa5ee278b96
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-03-06 16:06:44 +0100

    add categries images

--- delivery_app/lib/constants/colors.dart
+++ delivery_app/lib/constants/colors.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
 
-const Color kBackground = Color(0xFFF5F5F5);
-const Color kPrimaryColor = Color(0xFFFDBF30);
+/*const Color kBackground = Color(0xFFF5F5F5);
+const Color kPrimaryColor = Color(0xFFFDBF30);*/
+const Color kBackground = Color(0xFFFBE9E7);
+const Color kPrimaryColor =Color(0xFFFFCCBC);
