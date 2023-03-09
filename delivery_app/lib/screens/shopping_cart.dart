4ef5836005f8fa02b0da2c84b5a7081fc70bf9a1
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-03-09 11:36:41 +0100

    create shopping cart page

---
+++ delivery_app/lib/screens/shopping_cart.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class ShoppingCart extends StatelessWidget {
+  const ShoppingCart({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
