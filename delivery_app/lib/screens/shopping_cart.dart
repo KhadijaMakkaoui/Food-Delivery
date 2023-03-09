3eff68b770a734d9e01aea1b0f12b91b54a3db92
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-03-09 15:26:21 +0100

    display list of cart item

--- delivery_app/lib/screens/shopping_cart.dart
+++ delivery_app/lib/screens/shopping_cart.dart
@@ -1,9 +1,116 @@
+import 'package:delivery_app/constants/colors.dart';
 import 'package:flutter/material.dart';
-class ShoppingCart extends StatelessWidget {
+
+import '../models/cart_item.dart';
+class ShoppingCart extends StatefulWidget {
+
   const ShoppingCart({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+  State<ShoppingCart> createState() => _ShoppingCartState();
 }
+final List<CartItem> cartItems = [
+  CartItem(
+    name: 'Product 1',
+    price: 10.0,
+    imageUrl: 'https://via.placeholder.com/150',
+  ),
+  CartItem(
+    name: 'Product 2',
+    price: 20.0,
+    imageUrl: 'https://via.placeholder.com/150',
+  ),
+  CartItem(
+    name: 'Product 3',
+    price: 30.0,
+    imageUrl: 'https://via.placeholder.com/150',
+  ),
+  CartItem(
+    name: 'Product 4',
+    price: 40.0,
+    imageUrl: 'https://via.placeholder.com/150',
+  ),
+];
+
+class _ShoppingCartState extends State<ShoppingCart> {
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Container(
+            margin: EdgeInsets.only(top: 50),
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Cart',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '${cartItems.length} items',
+                      style: TextStyle(
+                        color: kGreen,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: cartItems.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/images/dish1.png',
+                            width: 80,
+                            height: 80,
+                          ),
+                          SizedBox(width: 20),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  cartItems[index].name,
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                Text(
+                                  '\$${cartItems[index].price}',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    color: kGreen,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+
+  }
+
