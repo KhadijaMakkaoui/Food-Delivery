a16e7760f8807767de1d7e650fb967ac3193fb07
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-03-13 15:59:56 +0100

    m add fromJson method

--- delivery_app/lib/models/restaurant.dart
+++ delivery_app/lib/models/restaurant.dart
@@ -8,20 +8,55 @@ class Restaurant{
 
   String logoUrl;
   String desc;
-  num score;
-  Map<String,List<Food>> menu;
+  String score;
+  /*Map<String,List<Food>> menu;*/
 
-  Restaurant(this.name, this.waitTime, this.distance, this.label, this.logoUrl,
-      this.desc, this.score, this.menu);
+  Restaurant(
+      {required this.name,
+        required this.waitTime,
+        required this.distance,
+        required this.label,
+        required this.logoUrl,
+        required this.desc,
+        required this.score} /*, this.menu*/);
+  static Restaurant fromJson(Map<String, dynamic> json){
+   return Restaurant(
+     name: json['name'] as String,
+     waitTime: json['waitTime'] as String,
+     distance: json['distance'] as String,
+     label: json['label'] as String,
+     logoUrl: json['logoUrl'] as String,
+     desc: json['desc'] as String,
+     score: json['score'] as String,
+   );
+  }
+  Map<String, dynamic> toJson() => {
+    'name': name,
+    'waitTime': waitTime,
+    'distance': distance,
+    'label': label,
+    'logoUrl': logoUrl,
+    'desc': desc,
+    'score': score,
+  };
   static Restaurant generateRestaurant(){
-    return Restaurant('Burger King', '20-30 min', '2.4 km', 'Restaurant', 'assets/images/res_logo.png', 'Orange sandwich is delicious', 4.7,
-      {
+      return Restaurant(
+        name: 'Burger King',
+        waitTime: '20-30 min',
+        distance: '2.4 km',
+        label: 'CCA23',
+        logoUrl: 'assets/images/burgerking.png',
+        desc: 'The best burger in town',
+        score: '4.5',
+        /*{
         'Recommended': Food.generateRecommendFoods(),
         'Popular': Food.generatePopularFoods(),
         'Noodles':[],
         'Pizza': []
-      }
+      }*/
     );
   }
+
+
 }
 
