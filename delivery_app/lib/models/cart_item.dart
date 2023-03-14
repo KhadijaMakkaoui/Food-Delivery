4629f79ced23cbeb9fcd27fe8e1cce13f55db7d4
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-03-14 09:22:03 +0100

    add quantity attribute

--- delivery_app/lib/models/cart_item.dart
+++ delivery_app/lib/models/cart_item.dart
@@ -1,16 +1,28 @@
 class CartItem {
-  final String name;
+  final String id;
+  final String title;
+  final int quantity;
   final double price;
   final String imageUrl;
 
-  CartItem({required this.name, required this.price, required this.imageUrl});
+  CartItem({required this.id,required this.title,required this.quantity, required this.price, required this.imageUrl});
 
   factory CartItem.fromJson(Map<String, dynamic> json) {
     return CartItem(
-      name: json['name'],
+      id: json['id'],
+      title: json['title'],
+      quantity: json['quantity'],
       price: json['price'],
       imageUrl: json['imageUrl'],
     );
   }
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'title': title,
+    'quantity': quantity,
+    'price': price,
+    'imageUrl': imageUrl,
+  };
 
 }
