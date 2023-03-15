1d1b6f8ba41ded88fc60171ea7dade19bf9b8239
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-03-15 16:28:51 +0100

    display all orders

--- delivery_app/lib/screens/orders.dart
+++ delivery_app/lib/screens/orders.dart
@@ -1,16 +1,76 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 class Orders extends StatefulWidget {
   const Orders({Key? key}) : super(key: key);
 
   @override
-  State<Orders> createState() => _OrdersState();
+  _OrdersState createState() => _OrdersState();
 }
 
 class _OrdersState extends State<Orders> {
+  late Stream<QuerySnapshot> _ordersStream;
+
+  @override
+  void initState() {
+    super.initState();
+    _ordersStream = FirebaseFirestore.instance.collection('orders').snapshots();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("orders"),
+      appBar: AppBar(
+        title: Text('Orders'),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _ordersStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          final List<QueryDocumentSnapshot>? documents = snapshot.data?.docs;
+          if (documents == null || documents.isEmpty) {
+            return Center(child: Text('No orders found'));
+          }
+
+          return ListView.builder(
+            itemCount: documents.length,
+            itemBuilder: (BuildContext context, int index) {
+              final Map<String, dynamic>? data = documents[index].data() as Map<String, dynamic>?;
+              if (data == null) {
+                return SizedBox();
+              }
+
+              final String orderId = documents[index].id;
+              final String date = (data['date'] as Timestamp).toDate().toString();
+              final List<dynamic>? items = data['items'];
+              final int? totalPrice = data['total'];
+
+              return ListTile(
+                title: Text('Order $orderId'),
+                subtitle: Text('Date: $date'),
+                trailing: Text('Total:'+ totalPrice.toString() + 'DH'),
+                onTap: () {
+                 /* Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => OrderDetailsPage(
+                        orderId: orderId,
+                        items: items?.map((item) => Food.fromMap(item)).toList(),
+                      ),
+                    ),
+                  );*/
+                },
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
---
+++ delivery_app/lib/services/orderService.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class OrderService extends StatefulWidget {
+  const OrderService({Key? key}) : super(key: key);
+
+  @override
+  State<OrderService> createState() => _OrderServiceState();
+}
+
+class _OrderServiceState extends State<OrderService> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
